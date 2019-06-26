2e2c0c70f9622b97e393612b4dc57e8035c515a2
Tammy Metz <tammy@tammymetz.com>
2019-06-26 21:36:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function justInvoke(fn) {
 }
 
 function setThisWithCall(fn, thisValue, arg) {
-  
+
 }
 
 function setThisWithApply(fn, thisValue, args) {
-  
+
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
-  
+
 }
